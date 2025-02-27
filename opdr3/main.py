db57573e90c7ffd85bd6e7b94940bf60b879c67b
Foppe Dijkstra <foppe.j.dijkstra@gmail.com>
2025-02-27 12:42:27 +0100

    fixe

--- opdr3/main.py
+++ opdr3/main.py
@@ -5,6 +5,8 @@ import numpy as np
 df = pandas.read_csv("../opdr3/data_clustering.csv", header=None)
 x = np.array(df)
 
+#print(np.append(x, [[3, 4]], axis=0))
+
 ## -2: VISITED
 ## -1: NOISE
 ## 0: UNVISITED
@@ -15,15 +17,15 @@ def distance(a, b) -> float:
 
 def DBSCAN(D, eps, MinPts):
     cluster = 0
-    labels = np.empty((200, 1), dtype=object)
     D = np.pad(D, ((0, 0), (0, 1)))
     i = 0
     while i < len(D):
         point = D[i]
         D[i, 2] = -2
         neighbor_pts = regionQuery(D, point, eps)
+        print("lenght neighbor_pts", len(neighbor_pts))
         if len(neighbor_pts) < MinPts:
-            labels[i, 2] = -1
+            D[i, 2] = -1
         else:
             cluster += 1
             expandCluster(D, point, neighbor_pts, cluster, eps, MinPts)
@@ -32,29 +34,30 @@ def DBSCAN(D, eps, MinPts):
 
 
 def regionQuery(D, P, eps):
-    N = []
+    N = np.array([])
     for other_point in D:
-        if other_point[2] == 0:
-            other_point[2] = -2
-        if distance(P, other_point) <= eps:
-            print("otherpoint, N:", other_point, N)
-            N = N.append(other_point)
-    print("N: ", N)
+        if distance(P[:2], other_point[:2]) <= eps:
+            #print("otherpoint, N:", other_point, N)
+            if len(N) == 0:
+                N = [other_point]
+            else:
+                N = np.append(N, [other_point], axis=0)
+    #print("N: ", N)
     return N
 
 
-def expandCluster(D, P, neighbor_points, C, eps, MinPts):
-    C = np.append(C, P)
-    print("nb points: ", neighbor_points)
+def expandCluster(D, P, neighbor_points, cluster, eps, MinPts):
+    P[2] = cluster
+    print("cluser", cluster)
+    #print("nb points: ", neighbor_points)
     for other_point in neighbor_points:
         if other_point[2] != 0:
             other_point[2] = -2
-            other_neighbor_points = regionQuery(D, other_point, eps)
-            if len(other_neighbor_points) >= MinPts:
-                neighbor_points = np.append(neighbor_points, other_point)
+            #other_neighbor_points = regionQuery(D, other_point, eps)
+            #if len(other_neighbor_points) >= MinPts:
+                #neighbor_points = np.append(neighbor_points, [other_point], axis=0)
         if other_point[2] <= 0: ## not part of any cluster
-            other_point[2] = C
+            other_point[2] = cluster
 
 
-
-DBSCAN(x, 0, 0)
+DBSCAN(x, 0.04, 3)
